3be3d84994ff218e5d7b62fd71e87c0a4906a58d
Krupskiy35 <34711961+Krupskiy35@users.noreply.github.com>
2020-04-03 15:11:43 +0300

    Add files via upload

---
+++ js/sc.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function() {
+
+    $("body").niceScroll({
+        scrollspeed: 60,
+
+        smoothscroll: true,
+        cursorwidth: 10,
+        cursorborder: 0,
+        cursorcolor: '#158ecc',
+        cursorborderradius: 2,
+        autohidemode: true,
+        horizrailenabled: true,
+        cursoropacitymin: 0,
+        background: false,
+        zindex: "9999"
+    });
+
+
+
+
+
+});
